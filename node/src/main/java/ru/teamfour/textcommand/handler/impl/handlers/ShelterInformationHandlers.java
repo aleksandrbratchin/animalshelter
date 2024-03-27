df6b07425f7811934658771a13fbb98cfcb5d8d1
korrova11 <korrova@yandex.ru>
2024-03-27 18:55:00 +0300

    add Command,  Handler, ShelterInformationsHandler

--- node/src/main/java/ru/teamfour/textcommand/handler/impl/handlers/ShelterInformationHandlers.java
+++ node/src/main/java/ru/teamfour/textcommand/handler/impl/handlers/ShelterInformationHandlers.java
@@ -12,19 +12,40 @@ public class ShelterInformationHandlers implements Handlers {
     public final Handler startHandler;
     public final Handler addressHandler;
     public final Handler workScheduleShelterHandler;
+    public final Handler drivingDirectionsHandler;
+    public final Handler safetyMeasuresInShelterHandler;
+    public final Handler securityDataHandler;
+    public final Handler contactForCommunicationHandler;
+    public final Handler volunteerHandler;
 
     public ShelterInformationHandlers(
-            Handler startHandler, @Qualifier("shelterAddressHandler") Handler addressHandler,
-            @Qualifier("workScheduleShelterHandler") Handler workScheduleShelterHandler) {
+            Handler startHandler,
+            @Qualifier("shelterAddressHandler") Handler addressHandler,
+            @Qualifier("workScheduleShelterHandler") Handler workScheduleShelterHandler,
+            @Qualifier("drivingDirectionsHandler") Handler drivingDirectionsHandler,
+            @Qualifier("safetyMeasuresInShelterHandler") Handler safetyMeasuresInShelterHandler,
+            @Qualifier("securityDataHandler") Handler securityDataHandler,
+            @Qualifier("contactForCommunicationHandler")Handler contactForCommunicationHandler,
+            @Qualifier("volunteerHandler")Handler volunteerHandler) {
         this.startHandler = startHandler;
         this.addressHandler = addressHandler;
         this.workScheduleShelterHandler = workScheduleShelterHandler;
+        this.drivingDirectionsHandler = drivingDirectionsHandler;
+        this.safetyMeasuresInShelterHandler = safetyMeasuresInShelterHandler;
+        this.securityDataHandler = securityDataHandler;
+        this.contactForCommunicationHandler = contactForCommunicationHandler;
+        this.volunteerHandler = volunteerHandler;
     }
 
     @Override
     public Handler getHandler() {
         startHandler.setNext(addressHandler);
         startHandler.setNext(workScheduleShelterHandler);
+        startHandler.setNext(drivingDirectionsHandler);
+        startHandler.setNext(safetyMeasuresInShelterHandler);
+        startHandler.setNext(securityDataHandler);
+        startHandler.setNext(contactForCommunicationHandler);
+        startHandler.setNext(volunteerHandler);
 
         return startHandler;
     }
