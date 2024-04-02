e4ac7b2c865fef04ea16315dfbd90b38acbea121
korrova11 <korrova@yandex.ru>
2024-04-02 22:14:57 +0300

    add sql

--- node/src/main/java/ru/teamfour/service/impl/shelter/ShelterServiceImpl.java
+++ node/src/main/java/ru/teamfour/service/impl/shelter/ShelterServiceImpl.java
@@ -21,6 +21,7 @@ public class ShelterServiceImpl implements ShelterService {
      */
     @Override
     public Shelter add(Shelter shelter) {
+
         return shelterRepository.save(shelter);
     }
 
