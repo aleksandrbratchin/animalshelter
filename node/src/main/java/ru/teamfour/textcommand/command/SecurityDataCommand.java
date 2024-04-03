c03e402f8ffbcee5aecc5fc610f3c33231ba076c
korrova11 <korrova@yandex.ru>
2024-04-03 13:31:19 +0300

    add function for key

--- node/src/main/java/ru/teamfour/textcommand/command/SecurityDataCommand.java
+++ node/src/main/java/ru/teamfour/textcommand/command/SecurityDataCommand.java
@@ -1,10 +1,12 @@
 package ru.teamfour.textcommand.command;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.teamfour.dao.entity.user.User;
+import ru.teamfour.service.impl.shelter.ShelterServiceImpl;
 import ru.teamfour.textcommand.command.api.AbstractTextCommand;
 import ru.teamfour.textcommand.command.api.State;
 
@@ -18,19 +20,25 @@ public class SecurityDataCommand extends AbstractTextCommand{
     @Value("${buttonName.securityData}")
     private String buttonName;
 
+    @Autowired
+    private ShelterServiceImpl shelterService;
+
     @Override
     public List<SendMessage> execute(CommandContext commandContext) {
-        User user = commandContext.getUser();
+
         Update update = commandContext.getUpdate();
         State state = State.INFO_SHELTER;//todo нужно еще проверок навесить
-        user.setState(state);
-        userService.save(user);
+
+
         //todo какие то действия
-        String answerMessage = "Answer: " + buttonName;
+        String answerMessage = "Данные охраны для оформлени пропуска на машину: " +
+                shelterService.findAll().get(0).getSecurityData();
+
         SendMessage sendMessage = messageUtils.generateSendMessageWithText(update, answerMessage);
         List<SendMessage> sendMessages = new ArrayList<>();
         sendMessages.add(addMenu(sendMessage, state));
         return sendMessages;
+
     }
 
     @Override
