d575af7ba0c8bbe947431180a6850d3c53da0657
aleks <alexbratchin@gmail.com>
2024-04-28 14:11:47 +0300

    Убрано боковое меню

--- node/src/main/resources/liquibase/scripts/animalshelter.sql
+++ node/src/main/resources/liquibase/scripts/animalshelter.sql
@@ -1,8 +1,17 @@
 -- liquibase formatted sql
 
--- changeset vpotapov:1
+-- changeset vpotapov:create_photo_report
 
-CREATE TABLE public.shelters
+CREATE TABLE photo_report
+(
+    id     uuid NOT NULL,
+    "data" bytea NULL,
+    CONSTRAINT photo_report_pkey PRIMARY KEY (id)
+);
+
+-- changeset vpotapov:create_shelters
+
+CREATE TABLE shelters
 (
     id              uuid         NOT NULL,
     about_shelter   text NULL,
@@ -15,12 +24,12 @@ CREATE TABLE public.shelters
     CONSTRAINT shelters_pkey PRIMARY KEY (id),
     CONSTRAINT shelters_type_of_animal_check CHECK (((type_of_animal)::text = ANY ((ARRAY['CAT':: character varying, 'DOG':: character varying])::text[])
 ) ),
-	CONSTRAINT uk_1nri2n2e9r4qk9ow6euk4qeb1 UNIQUE (name)
+	CONSTRAINT uk_name UNIQUE (name)
 );
 
--- changeset vpotapov:2
+-- changeset vpotapov:create_users
 
-CREATE TABLE public.users
+CREATE TABLE users
 (
     id           uuid         NOT NULL,
     date_create  timestamp(6) NULL,
@@ -35,43 +44,19 @@ CREATE TABLE public.users
     phone_number varchar(255) NULL,
     workload     int4 NULL,
     shelter_id   uuid NULL,
-    CONSTRAINT uk_nr2rmfhq6wfp39vcduy7iketb UNIQUE (chat_id),
+    CONSTRAINT uk_chat_id UNIQUE (chat_id),
     CONSTRAINT users_pkey PRIMARY KEY (id),
     CONSTRAINT users_role_user_check CHECK (((role_user)::text = ANY ((ARRAY['CLIENT':: character varying, 'VOLUNTEER':: character varying])::text[])
 ) ),
 	CONSTRAINT users_state_check CHECK (((state)::text = ANY ((ARRAY['INIT_MENU'::character varying, 'MAIN_MENU'::character varying, 'INFO_SHELTER'::character varying, 'ADOPTION'::character varying, 'CONTACT_VOLUNTEER_MENU'::character varying, 'VOLUNTEER_CHAT'::character varying, 'VOLUNTEER_START_MENU'::character varying, 'PET_REPORT'::character varying, 'RECOMMENDATIONS'::character varying, 'LEAVE_CONTACT_DETAILS_FOR_COMMUNICATION_MENU'::character varying, 'CLIENT_CHAT'::character varying, 'LIST_ANIMALS_MENU'::character varying, 'DAILY_REPORT_MENU'::character varying, 'WAITING_FOR_PHOTOS_FOR_DAILY_REPORT'::character varying, 'CHOOSE_SHELTER_MENU'::character varying, 'WAITING_TEXT_FOR_DAILY_REPORT'::character varying, 'CHECK_REPORT'::character varying])::text[])))
 );
 
-ALTER TABLE public.users
-    ADD CONSTRAINT fkkjrm7r3emrq99t5en9sjdsg5u FOREIGN KEY (shelter_id) REFERENCES public.shelters (id);
+ALTER TABLE users
+    ADD CONSTRAINT fk_users_shelters FOREIGN KEY (shelter_id) REFERENCES shelters (id);
 
--- changeset vpotapov:3
+-- changeset vpotapov:create_animals
 
-CREATE TABLE public.adoption_process_animal
-(
-    id                      uuid NOT NULL,
-    date_create             timestamp(6) NULL,
-    date_update             timestamp(6) NULL,
-    adoption_process_status varchar(255) NULL,
-    "date"                  date NOT NULL,
-    animal_id               uuid NULL,
-    shelter_id              uuid NULL,
-    user_id                 uuid NULL,
-    CONSTRAINT adoption_process_animal_adoption_process_status_check CHECK (((adoption_process_status)::text = ANY ((ARRAY['PROCESS_ADOPTION':: character varying, 'ADOPTED':: character varying, 'NOT_ADOPTED':: character varying, 'DOPTION_DENIED':: character varying])::text[])
-) ),
-	CONSTRAINT adoption_process_animal_pkey PRIMARY KEY (id)
-);
-
-ALTER TABLE public.adoption_process_animal
-    ADD CONSTRAINT fkcv8k8pjj6rhqgq13kjaotje71 FOREIGN KEY (animal_id) REFERENCES public.animals (id);
-ALTER TABLE public.adoption_process_animal
-    ADD CONSTRAINT fkfmccis708ahki042x3yfrgc1s FOREIGN KEY (shelter_id) REFERENCES public.shelters (id);
-ALTER TABLE public.adoption_process_animal
-    ADD CONSTRAINT fkq36alfrt46anaxkmal9pmqwtu FOREIGN KEY (user_id) REFERENCES public.users (id);
-
--- changeset vpotapov:4
-
-CREATE TABLE public.animals
+CREATE TABLE animals
 (
     id             uuid NOT NULL,
     adopted        varchar(255) NULL,
@@ -87,21 +72,36 @@ CREATE TABLE public.animals
 	CONSTRAINT animals_type_of_animal_check CHECK (((type_of_animal)::text = ANY ((ARRAY['CAT'::character varying, 'DOG'::character varying])::text[])))
 );
 
-ALTER TABLE public.animals
-    ADD CONSTRAINT fk7fmlpw3o4ourhtv3qy8gl6cn5 FOREIGN KEY (shelter_id) REFERENCES public.shelters (id);
+ALTER TABLE animals
+    ADD CONSTRAINT fk_animals FOREIGN KEY (shelter_id) REFERENCES shelters (id);
 
--- changeset vpotapov:5
+-- changeset vpotapov:create_adoption_process_animal
 
-CREATE TABLE public.photo_report
+CREATE TABLE adoption_process_animal
 (
-    id     uuid NOT NULL,
-    "data" bytea NULL,
-    CONSTRAINT photo_report_pkey PRIMARY KEY (id)
+    id                      uuid NOT NULL,
+    date_create             timestamp(6) NULL,
+    date_update             timestamp(6) NULL,
+    adoption_process_status varchar(255) NULL,
+    "date"                  date NOT NULL,
+    animal_id               uuid NULL,
+    shelter_id              uuid NULL,
+    user_id                 uuid NULL,
+    CONSTRAINT adoption_process_animal_adoption_process_status_check CHECK (((adoption_process_status)::text = ANY ((ARRAY['PROCESS_ADOPTION':: character varying, 'ADOPTED':: character varying, 'NOT_ADOPTED':: character varying, 'ADOPTION_DENIED':: character varying])::text[])
+) ),
+	CONSTRAINT adoption_process_animal_pkey PRIMARY KEY (id)
 );
 
--- changeset vpotapov:6
+ALTER TABLE adoption_process_animal
+    ADD CONSTRAINT fk_adoption_process_animal_animals FOREIGN KEY (animal_id) REFERENCES animals (id);
+ALTER TABLE adoption_process_animal
+    ADD CONSTRAINT fk_adoption_process_animal_shelters FOREIGN KEY (shelter_id) REFERENCES shelters (id);
+ALTER TABLE adoption_process_animal
+    ADD CONSTRAINT fk_adoption_process_animal_users FOREIGN KEY (user_id) REFERENCES users (id);
 
-CREATE TABLE public.daily_report
+-- changeset vpotapov:create_daily_report
+
+CREATE TABLE daily_report
 (
     id                 uuid         NOT NULL,
     date_create        timestamp(6) NULL,
@@ -115,33 +115,33 @@ CREATE TABLE public.daily_report
     CONSTRAINT daily_report_pkey PRIMARY KEY (id),
     CONSTRAINT daily_report_report_status_check CHECK (((report_status)::text = ANY ((ARRAY['IN_PROCESSING':: character varying, 'APPROVED':: character varying, 'NOT_APPROVED':: character varying])::text[])
 ) ),
-	CONSTRAINT uk_h27ociymu9o2g8i1pjxog27fp UNIQUE (photo_report_id)
+	CONSTRAINT uk_photo_report_id UNIQUE (photo_report_id)
 );
 
-ALTER TABLE public.daily_report
-    ADD CONSTRAINT fkd2rwr80f6v6w1tuil9u2a2lox FOREIGN KEY (adoption_animal_id) REFERENCES public.adoption_process_animal (id);
-ALTER TABLE public.daily_report
-    ADD CONSTRAINT fkeutnc4tinni81h3a4nuv3op5v FOREIGN KEY (volunteer_id) REFERENCES public.users (id);
-ALTER TABLE public.daily_report
-    ADD CONSTRAINT fkh5mc7nxb2eysi53u3wv90gmq0 FOREIGN KEY (photo_report_id) REFERENCES public.photo_report (id);
+ALTER TABLE daily_report
+    ADD CONSTRAINT fk_daily_report_adoption_process_animal FOREIGN KEY (adoption_animal_id) REFERENCES adoption_process_animal (id);
+ALTER TABLE daily_report
+    ADD CONSTRAINT fk_daily_report_users FOREIGN KEY (volunteer_id) REFERENCES users (id);
+ALTER TABLE daily_report
+    ADD CONSTRAINT fk_daily_report_photo_report FOREIGN KEY (photo_report_id) REFERENCES photo_report (id);
 
--- changeset vpotapov:7
+-- changeset vpotapov:create_driving_directions
 
-CREATE TABLE public.driving_directions
+CREATE TABLE driving_directions
 (
     id         uuid NOT NULL,
     "data"     bytea NULL,
     shelter_id uuid NULL,
     CONSTRAINT driving_directions_pkey PRIMARY KEY (id),
-    CONSTRAINT uk_cdlo00ydaxe0rykhwbhyvp592 UNIQUE (shelter_id)
+    CONSTRAINT uk_shelter_id UNIQUE (shelter_id)
 );
 
-ALTER TABLE public.driving_directions
-    ADD CONSTRAINT fkjopdd4vy6v7o2djebw0qwlgw5 FOREIGN KEY (shelter_id) REFERENCES public.shelters (id);
+ALTER TABLE driving_directions
+    ADD CONSTRAINT fk_driving_directions_shelters FOREIGN KEY (shelter_id) REFERENCES shelters (id);
 
--- changeset vpotapov:8
+-- changeset vpotapov:create_info_for_adoption
 
-CREATE TABLE public.info_for_adoption
+CREATE TABLE info_for_adoption
 (
     id                                            uuid         NOT NULL,
     home_improvement_for_adult_animal             text         NOT NULL,
@@ -157,5 +157,5 @@ CREATE TABLE public.info_for_adoption
     CONSTRAINT info_for_adoption_pkey PRIMARY KEY (id),
     CONSTRAINT info_for_adoption_type_of_animal_check CHECK (((type_of_animal)::text = ANY ((ARRAY['CAT':: character varying, 'DOG':: character varying])::text[])
 ) ),
-	CONSTRAINT uk_3aa48eh7ml1hhq7bslq6ypc38 UNIQUE (type_of_animal)
+	CONSTRAINT uk_type_of_animal UNIQUE (type_of_animal)
 );
